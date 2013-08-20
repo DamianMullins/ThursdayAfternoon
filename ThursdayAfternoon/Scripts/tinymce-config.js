4fc23b4a275031993006a4253431a2d6fb04f5a0
Damian Mullins <damian@lowflyingowls.co.uk>
2013-08-20 16:28:38 +0100

    Extracted tinymce config out into seperate file

---
+++ ThursdayAfternoon/Scripts/tinymce-config.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('textarea.tinymce').tinymce({
+        script_url: '/scripts/tinymce/tinymce.min.js',
+        schema: 'html5',
+        //inline: true,
+        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        menubar: false,
+        statusbar: false
+    });
+});
