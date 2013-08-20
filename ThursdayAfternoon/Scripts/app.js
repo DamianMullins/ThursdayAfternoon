88fc56a4baad69b2604a8c0343315ab0e686abee
Damian Mullins <damian@lowflyingowls.co.uk>
2013-08-20 14:39:08 +0100

    Wrote a basic fullscreen js method

--- ThursdayAfternoon/Scripts/app.js
+++ ThursdayAfternoon/Scripts/app.js
@@ -2,4 +2,10 @@
     if ($('form').length) {
         $('form:first').find('input:first').focus();
     }
+
+    $('.make-fullscreen').on('click', function (e) {
+        var target = $(this).data('target');
+        e.preventDefault();
+        $('.' + target).toggleClass('fullscreen gradient-bg');
+    });
 });
