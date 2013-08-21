2d68d1eb9a561a166e1239185b19e52a0f07bd72
Damian Mullins <damian@lowflyingowls.co.uk>
2013-08-21 14:30:55 +0100

    Updated tinymce package

--- ThursdayAfternoon/Scripts/tinymce/plugins/image/plugin.js
+++ ThursdayAfternoon/Scripts/tinymce/plugins/image/plugin.js
@@ -141,10 +141,12 @@ tinymce.PluginManager.add('image', function(editor) {
 			};
 
 			if (!imgElm) {
-				data.id = '__mcenew';
-				editor.insertContent(dom.createHTML('img', data));
-				imgElm = dom.get('__mcenew');
-				dom.setAttrib(imgElm, 'id', null);
+				editor.undoManager.transact(function() {
+					data.id = '__mcenew';
+					editor.insertContent(dom.createHTML('img', data));
+					imgElm = dom.get('__mcenew');
+					dom.setAttrib(imgElm, 'id', null);
+				});
 			} else {
 				dom.setAttribs(imgElm, data);
 			}
@@ -236,6 +238,8 @@ tinymce.PluginManager.add('image', function(editor) {
 
 			var data = win.toJSON();
 			var css = dom.parseStyle(data.style);
+
+			dom.setAttrib(imgElm, 'style', '');
 
 			delete css.margin;
 			css['margin-top'] = css['margin-bottom'] = addPixelSuffix(data.vspace);
